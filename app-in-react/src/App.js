cc762796fc3ed49411a6eb29ed18fc194bf153f4
leminhhieu98py@gmail.com <leminhhieu98py@gmail.com>
2020-11-24 14:14:15 +0700

    a

--- app-in-react/src/App.js
+++ app-in-react/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css';
-import CreateContainer from './Components/container'
+
 
 function App() {
   return (
     <React.Fragment>
-      <CreateContainer />
+
     </React.Fragment>
   );
 }
